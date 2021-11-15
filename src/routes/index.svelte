2ae4e6213645c3e54181a53e3e7d0641bc370d90
Sakulbuth Ekvittayaniphon <sakulbuth@gmail.com>
2021-11-15 15:55:05 +0700

    add index page

---
+++ src/routes/index.svelte
@@ -0,0 +1,10 @@
+<script lang=ts >
+	import { goto } from "@sapper/app";
+	
+	onMount(async() => {
+		goto('/round1/')
+	})
+</script>
+
+หน้านี้ว่างเปล่า <br/>
+กำลังพาไปยัง <a href='/round1'>หน้าแรก</a>
